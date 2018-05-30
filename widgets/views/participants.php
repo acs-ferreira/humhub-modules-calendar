672c0aea1a09c3211f2fa21b5f1995565e74ed6f
Carlos Ferreira <c.ferreira@acsiiweb.com>
2018-05-30 13:13:53 +0200

    Update participants.php

--- widgets/views/participants.php
+++ widgets/views/participants.php
@@ -1,4 +1,5 @@
 <?php
+
 use yii\helpers\Html;
 use humhub\modules\calendar\models\CalendarEntry;
 use humhub\modules\calendar\models\CalendarEntryParticipant;
@@ -8,16 +9,16 @@ use humhub\modules\calendar\models\CalendarEntryParticipant;
 <?php if ($calendarEntry->isParticipationAllowed()) : ?>
     <strong><i class="fa fa-users"></i> <?= Yii::t('CalendarModule.widgets_views_participants', 'Participants:'); ?></strong><br>
 
-    <?php $title = Yii::t('CalendarModule.widgets_views_participants', ":count attending", [':count' => $countAttending]); ?>
+    <?php $title = Yii::t('CalendarModule.widgets_views_participants', ':count attending', [':count' => $countAttending]); ?>
     <?php if ($countAttending > 0) : ?>
         <?= Html::a($title, $calendarEntry->content->container->createUrl('/calendar/entry/user-list', ['state' => CalendarEntryParticipant::PARTICIPATION_STATE_ACCEPTED, 'id' => $calendarEntry->id]), ["class" => "tt colorSuccess", "title" => "", "data-target" => '#globalModal', "data-placement" => "top", "data-original-title" => ""]) ?>
     <?php else : ?>
         <?= $title; ?>
     <?php endif ?>
 
-    <?php if($calendarEntry->allow_maybe) : ?>
+    <?php if ($calendarEntry->allow_maybe) : ?>
          &middot;
-        <?php $title = Yii::t('CalendarModule.widgets_views_participants', ":count maybe", [':count' => $countMaybe]); ?>
+        <?php $title = Yii::t('CalendarModule.widgets_views_participants', ':count maybe', [':count' => $countMaybe]); ?>
         <?php if ($countMaybe > 0) : ?>
             <?= Html::a($title, $calendarEntry->content->container->createUrl('/calendar/entry/user-list', ['state' => CalendarEntryParticipant::PARTICIPATION_STATE_MAYBE, 'id' => $calendarEntry->id]), ["class" => "tt colorInfo", "title" => "", "data-target" => '#globalModal', "data-placement" => "top", "data-original-title" => ""]); ?>
         <?php else : ?>
@@ -25,7 +26,7 @@ use humhub\modules\calendar\models\CalendarEntryParticipant;
         <?php endif ?>
     <?php endif ?>
 
-    <?php if($calendarEntry->allow_decline) : ?>
+    <?php if ($calendarEntry->allow_decline) : ?>
          &middot;
         <?php $title = Yii::t('CalendarModule.widgets_views_participants', ":count declined", [':count' => $countDeclined]); ?>
         <?php if ($countDeclined > 0) : ?>
@@ -34,6 +35,5 @@ use humhub\modules\calendar\models\CalendarEntryParticipant;
             <?= $title; ?>
         <?php endif ?>
     <?php endif ?>
-
     <br>
 <?php endif; ?>
