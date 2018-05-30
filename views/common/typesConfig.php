bf7e1a005e8924c91a68af73d65377ddfd9ab6b8
Carlos Ferreira <c.ferreira@acsiiweb.com>
2018-05-30 13:25:51 +0200

    Update typesConfig.php

--- views/common/typesConfig.php
+++ views/common/typesConfig.php
@@ -6,6 +6,7 @@
  * @license https://www.humhub.com/licences
  *
  */
+
 /* @var $this yii\web\View */
 /* @var $typeDataProvider \yii\data\ActiveDataProvider */
 /* @var $contentContainer \humhub\modules\content\components\ContentContainerActiveRecord */
@@ -18,30 +19,31 @@ use humhub\widgets\ActiveForm;
 use humhub\widgets\Button;
 use humhub\widgets\ModalButton;
 use humhub\widgets\Tabs;
-use \yii\helpers\Html;
+use yii\helpers\Html;
 use yii\widgets\ListView;
-
 ?>
 
 <div class="panel panel-default">
 
-    <div class="panel-heading"><?= Yii::t('CalendarModule.config', '<strong>Calendar</strong> module configuration'); ?></div>
+    <div class="panel-heading">
+        <?= Yii::t('CalendarModule.config', '<strong>Calendar</strong> module configuration'); ?>
+    </div>
 
     <?php if($contentContainer === null) : ?>
-        <?= GlobalConfigMenu::widget() ?>
+        <?= GlobalConfigMenu::widget(); ?>
     <?php else: ?>
-        <?= ContainerConfigMenu::widget()?>
+        <?= ContainerConfigMenu::widget(); ?>
     <?php endif; ?>
 
     <div class="panel-body">
         <div class="clearfix">
             <h4>
                 <?= Yii::t('CalendarModule.config', 'Event Type Configuration'); ?>
-            <?= ModalButton::success(Yii::t('CalendarModule.config', 'Create new type'))->load($createUrl)->icon('fa-plus')->right(); ?>
+                <?= ModalButton::success(Yii::t('CalendarModule.config', 'Create new type'))->load($createUrl)->icon('fa-plus')->right(); ?>
             </h4>
 
             <div class="help-block">
-                <?= Yii::t('CalendarModule.config', 'Here you can manage your event types.') ?>
+                <?= Yii::t('CalendarModule.config', 'Here you can manage your event types.'); ?>
             </div>
 
         </div>
@@ -54,7 +56,8 @@ use yii\widgets\ListView;
                         'contentContainer' => $contentContainer
                 ],
                 'emptyText' => Yii::t('CalendarModule.config', 'There are currently no event types available.')
-            ])?>
+            ]);
+            ?>
         </div>
 
     </div>
