8c8237dc53ca2122f90aa0bc83b05361c7ec73b7
Carlos Ferreira <c.ferreira@acsiiweb.com>
2018-05-30 13:28:19 +0200

    Update defaultConfig.php

--- views/common/defaultConfig.php
+++ views/common/defaultConfig.php
@@ -9,29 +9,32 @@
 /* @var $this yii\web\View */
 /* @var $model \humhub\modules\calendar\models\DefaultSettings */
 
-\humhub\modules\calendar\assets\Assets::register($this);
-
+use humhub\modules\calendar\assets\Assets;
 use humhub\modules\calendar\models\forms\CalendarEntryForm;
 use humhub\modules\calendar\widgets\ContainerConfigMenu;
 use humhub\modules\calendar\widgets\GlobalConfigMenu;
 use humhub\widgets\ActiveForm;
 use humhub\widgets\Button;
 use humhub\widgets\Tabs;
-use \yii\helpers\Html;
+use yii\helpers\Html;
+
+Assets::register($this);
 
 $helpBlock = $model->isGlobal()
     ? Yii::t('CalendarModule.config', 'Here you can configure default settings for new calendar events. These settings can be overwritten on space/profile level.')
-    : Yii::t('CalendarModule.config', 'Here you can configure default settings for new calendar events.') ;
+    : Yii::t('CalendarModule.config', 'Here you can configure default settings for new calendar events.');
 ?>
 
 <div class="panel panel-default">
 
-    <div class="panel-heading"><?= Yii::t('CalendarModule.config', '<strong>Calendar</strong> module configuration'); ?></div>
+    <div class="panel-heading">
+        <?= Yii::t('CalendarModule.config', '<strong>Calendar</strong> module configuration'); ?>
+    </div>
 
-    <?php if($model->isGlobal()) : ?>
-        <?= GlobalConfigMenu::widget() ?>
+    <?php if ($model->isGlobal()) : ?>
+        <?= GlobalConfigMenu::widget(); ?>
     <?php else: ?>
-        <?= ContainerConfigMenu::widget()?>
+        <?= ContainerConfigMenu::widget(); ?>
     <?php endif; ?>
 
     <div class="panel-body" data-ui-widget="calendar.Form">
@@ -44,15 +47,15 @@ $helpBlock = $model->isGlobal()
                 <?= $helpBlock ?>
             </div>
 
-            <?= $form->field($model, 'participation_mode')->dropDownList(CalendarEntryForm::getParticipationModeItems(), ['data-action-change' => 'changeParticipationMode'])?>
+            <?= $form->field($model, 'participation_mode')->dropDownList(CalendarEntryForm::getParticipationModeItems(), ['data-action-change' => 'changeParticipationMode']); ?>
             <div class="participationOnly" style="<?= $model->isParticipationAllowed() ? '' : 'display:none' ?>">
-                <?= $form->field($model, 'allow_decline')->checkbox() ?>
-                <?= $form->field($model, 'allow_maybe')->checkbox() ?>
+                <?= $form->field($model, 'allow_decline')->checkbox(); ?>
+                <?= $form->field($model, 'allow_maybe')->checkbox(); ?>
             </div>
 
-            <?= Button::primary(Yii::t('base', 'Save'))->submit() ?>
+            <?= Button::primary(Yii::t('base', 'Save'))->submit(); ?>
 
-            <?php if($model->showResetButton()) : ?>
+            <?php if ($model->showResetButton()) : ?>
                 <a href="<?= $model->getResetButtonUrl(); ?>" class='btn btn-default pull-right' data-ui-loader><?= Yii::t('CalendarModule.config', 'Reset'); ?></a>
             <?php endif; ?>
         <?php ActiveForm::end(); ?>
